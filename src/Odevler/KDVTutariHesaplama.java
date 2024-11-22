79bd715f5370c7da27aad261e699265d002ec1da
suzunn <slymnuzn@hotmail.com>
2024-11-22 15:05:16 +0300

    odevler2

--- src/Odevler/KDVTutariHesaplama.java
+++ src/Odevler/KDVTutariHesaplama.java
@@ -1,4 +1,18 @@
 package Odevler;
 
+import java.util.Scanner;
+
 public class KDVTutariHesaplama {
+    public static void main(String[] Args){
+        float KDV = 18/100f, fiyat, kdvli;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ürün fiyatını girin: ");
+        fiyat = sc.nextFloat();
+        kdvli = fiyat*KDV;
+
+        System.out.printf("KDV'siz fiyat: %.2f\n", fiyat);
+        System.out.printf("KDV fiyatı: %.2f\n", kdvli);
+        System.out.printf("KDV'li fiyat: %.2f\n", (fiyat + kdvli));
+
+    }
 }
