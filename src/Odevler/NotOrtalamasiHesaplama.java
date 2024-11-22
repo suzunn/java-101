69ccd85b7c864833facec5607cd68cad1e852f54
suzunn <slymnuzn@hotmail.com>
2024-11-22 14:40:08 +0300

    odevler

--- src/Odevler/NotOrtalamasiHesaplama.java
+++ src/Odevler/NotOrtalamasiHesaplama.java
@@ -1,4 +1,4 @@
-package Programlar;
+package Odevler;
 
 import java.util.Scanner;
 
--- src/Odevler/dikucgen.java
+++
@@ -1,8 +0,0 @@
-package Programlar;
-
-public class dikucgen {
-    public static void main(String[] Args){
-        float a = 10.323f;
-        System.out.printf("%f",a);
-    }
-}
